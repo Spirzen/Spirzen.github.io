e616ae02eeb36043b71e71c74cc42885d6c8649f
Spirzen <tim.tagirov@mail.ru>
2025-05-13 00:30:32 +0500

    Мелкие улучшения

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-// Логика переключения вкладок
-document.querySelectorAll('.tab-link').forEach(link => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault();
-  
-      // Убираем активный класс у всех ссылок
-      document.querySelectorAll('.tab-link').forEach(item => item.classList.remove('active'));
-  
-      // Добавляем активный класс текущей ссылке
+document.querySelectorAll('.tab-link, .tab-switch').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelectorAll('.tab-link').forEach(item => item.classList.remove('active'));
+
+    document.querySelectorAll('.tab-pane').forEach(tab => tab.classList.remove('active'));
+
+    const tabId = this.getAttribute('data-tab');
+    document.getElementById(tabId).classList.add('active');
+
+    if (this.classList.contains('tab-link')) {
       this.classList.add('active');
-  
-      // Скрываем все вкладки
-      document.querySelectorAll('.tab-pane').forEach(tab => tab.classList.remove('active'));
-  
-      // Показываем выбранную вкладку
-      const tabId = this.getAttribute('data-tab');
-      document.getElementById(tabId).classList.add('active');
-    });
+    }
   });
+});
